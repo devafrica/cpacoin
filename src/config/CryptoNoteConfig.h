bb998eee37dc85ad8bbf655f6916a38d5a23b2df
devafrica <cdevafrica@gmail.com>
2020-09-21 12:18:56 +0200

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -44,7 +44,7 @@ const uint64_t LWMA_2_DIFFICULTY_BLOCK_INDEX_V3              = 130;
 const unsigned EMISSION_SPEED_FACTOR                         = 19;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
-/* Premine amount */
+/* Premine amount WHY? because we swapped network So dont bash the coin */
 const uint64_t GENESIS_BLOCK_REWARD                          = UINT64_C(50000000000000);
 
 const char     GENESIS_COINBASE_TX_HEX[]   = "013201ff000180c0f4c198af0b02d393c988ad557b7cf8d94d7399751b3174fd62eaf9cbf8061c862bfa234958e12101422eca328fb0226976770cf10683a793046dc149a72d93e2e8f0d1a88f69715b";
@@ -137,7 +137,7 @@ const uint64_t MAX_EXTRA_SIZE_V2_HEIGHT                      = 500;
 
 
 /* For new projects forked from this code base, the values immediately below
-   should be changed to 0 to prevent issues with transaction processing 
+   should be changed to 0 to prevent issues with transaction processing
    and other possible unexpected behavior */
 const uint64_t TRANSACTION_SIGNATURE_COUNT_VALIDATION_HEIGHT = 15;
 const uint64_t BLOCK_BLOB_SHUFFLE_CHECK_HEIGHT               = 16;
@@ -194,7 +194,7 @@ const uint64_t FORK_HEIGHTS[] =
     80,  // 5
     100, // 6
     120, // 7
-   
+
 };
 
 /* MAKE SURE TO UPDATE THIS VALUE WITH EVERY MAJOR RELEASE BEFORE A FORK */
